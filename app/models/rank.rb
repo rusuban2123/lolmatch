690fc4bcad8f3544c65db338a300bd79c6cc2fa8
rusuban2123 <shin12032123@gmail.com>
2020-11-05 06:22:57 +0900

    アクティブハッシュとパラメーター許可

---
+++ app/models/rank.rb
@@ -0,0 +1,11 @@
+class Rank < ActiveHash::Base
+  self.data = [
+    { id: 1, name: 'iron4' }, { id: 2, name: 'iron3' }, { id: 3, name: 'iron2' }, { id: 4, name: 'iron1' }, 
+    { id: 5, name: 'bronze4' }, { id: 6, name: 'bronze3' }, { id: 7, name: 'bronze2' }, { id: 8, name: 'bronze1' }, 
+    { id: 9, name: 'silver4' }, { id: 10, name: 'silver3' }, { id: 11, name: 'silver2' }, { id: 12, name: 'silver1' }, 
+    { id: 13, name: 'gold4' }, { id: 14, name: 'gold3' }, { id: 15, name: 'gold2' }, { id: 16, name: 'gold1' }, 
+    { id: 17, name: 'plat4' }, { id: 18, name: 'plat3' }, { id: 19, name: 'plat2' }, { id: 20, name: 'plat1' }, 
+    { id: 21, name: 'dia4' }, { id: 22, name: 'dia3' }, { id: 23, name: 'dia2' }, { id: 24, name: 'dia1' },
+    { id: 25, name: 'master' }, { id: 26, name: 'grandmaster' }, { id: 27, name: 'challenger' }
+  ]
+end
