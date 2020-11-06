9c4ce7127c71eb2461f169d5d3a99c3d873e6335
rusuban2123 <shin12032123@gmail.com>
2020-11-06 11:34:41 +0900

    申請表示

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,6 +8,10 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:id])
     redirect_to root_path
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
   resources :tops
   resources :users
+  resources :relationships
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
